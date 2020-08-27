3aeecd191c26aacfb3370db5d567f241a2bfa02b
Jason Qiu <jason.qiu@duke.edu>
2020-08-27 17:14:23 -0400

    2nd question

--- src/names/Main.java
+++ src/names/Main.java
@@ -30,19 +30,25 @@ public class Main {
                 break;
             }
         }
-//
-//        System.out.println("2."); // question 2
-//        Scanner sc = new Scanner(new File("data/yob1900.txt"));
-//        while(sc.hasNextLine()) {
-//            String[] nameArray = scanner.nextLine().split(",");
-//            if (!firstFemale && nameArray[1].equals("F")) {
-//                firstFemale = true;
-//                System.out.println(nameArray[0]);
-//            }
-//            else if (nameArray[1].equals("M")) {
-//                System.out.println(nameArray[0]);
-//                break;
-//            }
-//        }
+
+        System.out.println("2."); // question 2
+        System.out.print("Year: ");
+        year = scan.next();
+        System.out.print("Gender (M/F): ");
+        String gender = scan.next();
+        System.out.print("First Letter: ");
+        String letter = scan.next();
+        Scanner sc = new Scanner(new File("data/ssa_complete/yob" + year + ".txt"));
+        int count = 0;
+        int totalBabies = 0;
+        while(sc.hasNextLine()) {
+            String[] nameArray = sc.nextLine().split(",");
+            if (nameArray[1].equals(gender) && nameArray[0].substring(0,1).equals(letter)) {
+                count++;
+                totalBabies += Integer.valueOf(nameArray[2]);
+            }
+        }
+        System.out.println(count);
+        System.out.println(totalBabies);
     }
 }
