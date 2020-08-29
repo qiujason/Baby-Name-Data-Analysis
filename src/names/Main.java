a4ed5be51a9a1f3ea9c2d6a309af120b4a46b2ec
Jason Qiu <jason.qiu@duke.edu>
2020-08-29 19:36:20 -0400

    finish method for getting rankings of all years by name and gender

--- src/names/Main.java
+++ src/names/Main.java
@@ -3,6 +3,8 @@ package names;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -12,19 +14,26 @@ public class Main {
     /**
      * Start of the program.
      */
+    private static final int FIRSTYEAR = 1880;
+    private static final int LASTYEAR = 2018;
+
     // test implementation #1
     private static final int YEAR1 = 1900;
 
     // test implementation #2
     private static final int YEAR2 = 1900;
-    private static final String GENDER = "F";
+    private static final String GENDER1 = "F";
     private static final String LETTER = "Q";
+
+    // basic implementation #1
+    private static final String NAME1 = "John";
+    private static final String GENDER2 = "M";
     
-    private static String[] getTopRankedYear(int year) throws FileNotFoundException {
+    public static String[] getTopRankedYear(int year) throws FileNotFoundException {
         String[] topRanked = new String[2];
         Scanner scanner = new Scanner(new File("data/ssa_complete/yob" + year + ".txt"));
         boolean firstFemale = false;
-        while(scanner.hasNextLine()) {
+        while (scanner.hasNextLine()) {
             String[] nameArray = scanner.nextLine().split(",");
             if (!firstFemale && nameArray[1].equals("F")) {
                 firstFemale = true;
@@ -38,25 +47,52 @@ public class Main {
         return topRanked;
     }
 
-    private static int[] getCountByGenderLetterYear(int year, String gender, String letter) throws FileNotFoundException {
+    public static int[] getCountByGenderLetterYear(int year, String gender, String letter) throws FileNotFoundException {
         int[] counts = new int[2];
         Scanner scanner = new Scanner(new File("data/ssa_complete/yob" + year + ".txt"));
-        while(scanner.hasNextLine()) {
+        while (scanner.hasNextLine()) {
             String[] nameArray = scanner.nextLine().split(",");
             if (nameArray[1].equals(gender) && nameArray[0].substring(0,1).equals(letter)) {
                 counts[0]++;
-                counts[1] += Integer.valueOf(nameArray[2]);
+                counts[1] += Integer.parseInt(nameArray[2]);
             }
         }
         return counts;
     }
+
+    public static Map<Integer,Integer> getRankingsNameGender(String name, String gender) throws FileNotFoundException {
+        Map<Integer, Integer> rankings = new HashMap<>();
+        for (int year = FIRSTYEAR; year <= LASTYEAR; year++) {
+            Scanner scanner = new Scanner(new File("data/ssa_complete/yob" + year + ".txt"));
+            boolean found = false;
+            while (scanner.hasNextLine()) {
+                String[] nameArray = scanner.nextLine().split(",");
+                if (nameArray[1].equals(gender) && nameArray[0].equals(name)) {
+                    rankings.put(year, Integer.parseInt(nameArray[2]));
+                    found = true;
+                    break;
+                }
+            }
+            // name not found
+            if (!found) rankings.put(year, -1);
+        }
+        return rankings;
+    }
+
     public static void main (String[] args) throws FileNotFoundException {
         System.out.println("Test Implementation #1");
+        System.out.println(YEAR1);
         for (String s : getTopRankedYear(YEAR1)) System.out.println(s);
 
         System.out.println("\nTest Implementation #2");
-        int[] counts = getCountByGenderLetterYear(YEAR2, GENDER, LETTER);
+        System.out.println(YEAR2 + " - " + LETTER + " (" + GENDER1 + ")");
+        int[] counts = getCountByGenderLetterYear(YEAR2, GENDER1, LETTER);
         System.out.println(counts[0] + " different names");
         System.out.println(counts[1] + " total babies");
+
+        System.out.println("\nBasic Implementation #1");
+        System.out.println(NAME1 + " (" + GENDER2 + ")");
+        Map<Integer, Integer> rankings = getRankingsNameGender(NAME1, GENDER2);
+        for (int year : rankings.keySet()) System.out.println(year + " - " + rankings.get(year));
     }
 }
