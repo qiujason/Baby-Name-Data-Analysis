d9ea70280d9f1460669d6945f0fc7f786fcdc251
Jason Qiu <jason.qiu@duke.edu>
2020-08-31 00:18:35 -0400

    finish getting top rank names over range of time

--- src/names/Main.java
+++ src/names/Main.java
@@ -3,9 +3,7 @@ package names;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Feel free to completely change this code or delete it entirely. 
@@ -36,8 +34,10 @@ public class Main {
     private final String GENDER3 = "M";
 
     // basic implementation #3
+    private final int TOPRANKS = 10;
     private final int START_YEAR = 2000;
     private final int FINAL_YEAR = 2009;
+    private final String GENDER4 = "M";
     
     public String[] getTopRankedYear(int year) throws FileNotFoundException {
         String[] topRanked = new String[2];
@@ -117,6 +117,23 @@ public class Main {
         return "Name not found";
     }
 
+    public void getMostPopularInRange(Map<String, Integer> ranking, Map<String, Integer> years, String gender, int startyear, int finalyear) throws FileNotFoundException {
+        for (int year = startyear; year < finalyear; year++) {
+            Scanner scanner = new Scanner(new File("data/ssa_complete/yob" + year + ".txt"));
+            int rank = 0;
+            while (scanner.hasNextLine() && rank < TOPRANKS) {
+                String[] nameArray = scanner.nextLine().split(",");
+                if (nameArray[1].equals(gender)) {
+                    ranking.putIfAbsent(nameArray[0], 0);
+                    ranking.put(nameArray[0], ranking.get(nameArray[0]) + rank);
+                    years.putIfAbsent(nameArray[0], 0);
+                    years.put(nameArray[0], years.get(nameArray[0]) + 1);
+                    rank++;
+                }
+            }
+        }
+    }
+
     public static void main (String[] args) throws FileNotFoundException {
         Main main = new Main();
 
@@ -137,5 +154,20 @@ public class Main {
 
         System.out.println("\nBasic Implementation #2");
         System.out.println(main.getSameRank(main.NAME2, main.GENDER3, main.YEAR3));
+
+        System.out.println("\nBasic Implementation #3");
+        Map<String, Integer> ranking = new HashMap<>();
+        Map<String, Integer> years = new HashMap<>();
+        main.getMostPopularInRange(ranking, years, main.GENDER4, main.START_YEAR, main.FINAL_YEAR);
+        SortedSet<String> keys = new TreeSet<>((a,b)->{
+            int val = years.get(b) - years.get(a);
+            return val != 0 ? val : ranking.get(a)-ranking.get(b);
+        });
+        keys.addAll(years.keySet());
+        int i = 1;
+        for (String key : keys) {
+            System.out.println(i + ". " + key + " for " + years.get(key) + " years");
+            i++;
+        }
     }
 }
