2e44ecb47d9a973221d16b2542a3aae898d4823e
Jason Qiu <jason.qiu@duke.edu>
2020-08-31 14:55:21 -0400

    create test file

--- test/names/MainTest.java
+++ test/names/MainTest.java
@@ -1,4 +1,36 @@
+package names;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+    Main main = new Main();
+
+    @Test
+    void testGetTopRankedYear() throws FileNotFoundException {
+        assertArrayEquals(new String[]{"Mary", "John"}, main.getTopRankedYear(1900));
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCountByGenderLetterYear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRankingsNameGender() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSameRank() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMostPopularInRange() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void mostPopularLetterGirls() {
+    }
 }
