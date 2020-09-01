45190ab2fb13bd1a9d20aa39374fc766d1675a4f
Jason Qiu <jason.qiu@duke.edu>
2020-09-01 00:44:02 -0400

    add tests for count by gender letter year and most popular letter for girls in range

--- test/names/MainTest.java
+++ test/names/MainTest.java
@@ -21,27 +21,27 @@ class MainTest {
     }
     @Test
     void testGetTopRankedYear2021() throws FileNotFoundException {
-        assertArrayEquals(new String[]{"Emma", "Liam"}, testInstance.getTopRankedYear(2021));
+        assertArrayEquals(new String[]{"Clara", "John"}, testInstance.getTopRankedYear(2021));
     }
     @Test
     void testGetTopRankedYear2022() throws FileNotFoundException {
-        assertArrayEquals(new String[]{"Emma", "Liam"}, testInstance.getTopRankedYear(2022));
+        assertArrayEquals(new String[]{"Emma", "John"}, testInstance.getTopRankedYear(2022));
     }
 
     @Test
-    void testGetCountByGenderLetterYear2020() throws FileNotFoundException {
+    void testGetCountByGenderLetterYearFemale() throws FileNotFoundException {
         assertArrayEquals(new int[]{2, 110000}, testInstance.getCountByGenderLetterYear(2020, "F", "A"));
     }
 
-//    @Test
-//    void testGetCountByGenderLetterYear() throws FileNotFoundException {
-//
-//    }
-//
-//    @Test
-//    void testGetCountByGenderLetterYear() throws FileNotFoundException {
-//
-//    }
+    @Test
+    void testGetCountByGenderLetterYearNone() throws FileNotFoundException {
+        assertArrayEquals(new int[]{0, 0}, testInstance.getCountByGenderLetterYear(2021, "M", "Z"));
+    }
+
+    @Test
+    void testGetCountByGenderLetterYearMale() throws FileNotFoundException {
+        assertArrayEquals(new int[]{1, 20000}, testInstance.getCountByGenderLetterYear(2022, "M", "M"));
+    }
 //
 //    @Test
 //    void testGetRankingsNameGender() {
@@ -72,7 +72,8 @@ class MainTest {
 
     @Test
     void testGetMostPopularInRange20_22() throws FileNotFoundException {
-        assertArrayEquals(new String[]{"Emma for 2 years"}, testInstance.getMostPopularInRange("F", 2020, 2022).toArray());
+        assertArrayEquals(new String[]{"Emma for 2 years"},
+                testInstance.getMostPopularInRange("F", 2020, 2022).toArray());
     }
 
     @Test
@@ -83,21 +84,24 @@ class MainTest {
 
     @Test
     void testGetMostPopularInRange22_22() throws FileNotFoundException {
-        assertArrayEquals(new String[]{"John for 1 years"}, testInstance.getMostPopularInRange("M", 2022, 2022).toArray());
+        assertArrayEquals(new String[]{"John for 1 years"},
+                testInstance.getMostPopularInRange("M", 2022, 2022).toArray());
     }
-//
-//    @Test
-//    void mostPopularLetterGirls() {
-//
-//    }
-//
-//    @Test
-//    void mostPopularLetterGirls() {
-//
-//    }
-//
-//    @Test
-//    void mostPopularLetterGirls() {
-//
-//    }
+
+    @Test
+    void testMostPopularLetterGirls22() throws FileNotFoundException {
+        assertArrayEquals(new String[]{"A", "Amelia", "Ava"}, testInstance.mostPopularLetterGirls(2022, 2022));
+    }
+
+    @Test
+    void testMostPopularLetterGirls20_22() throws FileNotFoundException {
+        assertArrayEquals(new String[]{"E", "Elizabeth", "Emma", "Evelyn"},
+                testInstance.mostPopularLetterGirls(2020, 2022));
+    }
+
+    @Test
+    void mostPopularLetterGirls21_22() throws FileNotFoundException {
+        assertArrayEquals(new String[]{"C", "Charlotte", "Clara"},
+                testInstance.mostPopularLetterGirls(2021, 2022));
+    }
 }
